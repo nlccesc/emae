b261ea993750703048f2b5ca2608dfadb7f18d32
nlccesc <93630735+nlccesc@users.noreply.github.com>
2024-04-13 16:34:57 +0800

    Update and rename adaptive_encryption.py to test2.py
    moved adaptive enigma and huffman encoding/decoding in individual files. 
    
    file left for testing

---
+++ test2.py
@@ -0,0 +1,6 @@
+import random
+import math
+from collections import Counter
+from heapq import heapify, heappop, heappush
+from huffmantree import HuffmanTree
+from adaptivenigma import AdaptiveEnigma
